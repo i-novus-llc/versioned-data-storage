4e40e37016bb85192d907e8e686044a1f5bda651
Alexander Belov <abelov@i-novus.ru>
2020-06-11 09:47:01 +0300

    [CF] Build all branches. Deploy tags only.

--- .travis/build.sh
+++ .travis/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [ $TRAVIS_TAG == '' ] ; then
+if [[ $TRAVIS_TAG == '' ]] ; then
   mvn --settings .maven.xml clean install -B -U
 else
   mvn --settings .maven.xml clean deploy -DskipTests=true -Prelease -B -U
