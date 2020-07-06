d8d7d7daa2bfd0dda6c9b9d8e5d06817aaf7eac9
Alexander Belov <abelov@i-novus.ru>
2020-07-06 10:15:56 +0300

    cf fix build

--- .travis/build.sh
+++ .travis/build.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
 printenv | grep TRAVIS_TAG; if [[ $TRAVIS_TAG == '' ]] ; then
-  echo "TAG: $TRAVIS_TAG"
   mvn --settings .maven.xml clean install -B -U
 else
-  echo "TAG: $TRAVIS_TAG"
   mvn --settings .maven.xml clean deploy -DskipTests=true -Prelease -B -U
 fi
