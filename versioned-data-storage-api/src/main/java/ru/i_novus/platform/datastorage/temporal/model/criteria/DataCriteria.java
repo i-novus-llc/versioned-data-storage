1f10aa1135028e2d92a4c41d00478b6fcc27e92a
arahmatullin <arahmatullin@i-novus.ru>
2020-08-12 09:16:45 +0300

    cf RDM-780 уточнение критерия поиска.

--- versioned-data-storage-api/src/main/java/ru/i_novus/platform/datastorage/temporal/model/criteria/DataCriteria.java
+++ versioned-data-storage-api/src/main/java/ru/i_novus/platform/datastorage/temporal/model/criteria/DataCriteria.java
@@ -5,9 +5,12 @@ import net.n2oapp.criteria.api.Criteria;
 /** Критерий поиска данных. */
 public class DataCriteria extends Criteria {
 
-    // Нумерация страниц с 1.
+    // Нумерация страниц с 1, поэтому сдвиг = +1.
+    public static final int PAGE_SHIFT = 1;
+
     public static final int MIN_PAGE = 1;
     public static final int MIN_SIZE = 1;
+
     public static final int NO_PAGINATION_PAGE = 0;
     public static final int NO_PAGINATION_SIZE = 0;
 
@@ -23,6 +26,6 @@ public class DataCriteria extends Criteria {
         if (getPage() < MIN_PAGE || getSize() < MIN_SIZE)
             throw new IllegalStateException("Criteria page and size should be greater than zero");
 
-        return (getPage() - 1) * getSize();
+        return (getPage() - MIN_PAGE) * getSize();
     }
 }
